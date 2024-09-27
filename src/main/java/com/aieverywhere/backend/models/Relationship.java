ed10cf227e736586cb73463999ea5d99f743c3e8
Daniel Hsu <hsu2005715@gmail.com>
2024-09-27 16:30:21 +0800

    新增實體類relationship

---
+++ src/main/java/com/aieverywhere/backend/models/Relationship.java
@@ -0,0 +1,64 @@
+package com.aieverywhere.backend.models;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Relationship {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long relationshipId;
+	private Long userId;
+	private Long friendId;
+	@Enumerated(EnumType.STRING)
+	private RelationshipStatus relationshipStatus;
+	
+	public Relationship() {
+		
+	}
+
+	public Relationship(Long relationshipId, Long userId, Long friendId, RelationshipStatus relationshipStatus) {
+		this.relationshipId = relationshipId;
+		this.userId = userId;
+		this.friendId = friendId;
+		this.relationshipStatus = relationshipStatus;
+	}
+
+	public Long getRelationshipId() {
+		return relationshipId;
+	}
+
+	public void setRelationshipId(Long relationshipId) {
+		this.relationshipId = relationshipId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Long getFriendId() {
+		return friendId;
+	}
+
+	public void setFriendId(Long friendId) {
+		this.friendId = friendId;
+	}
+
+	public RelationshipStatus getRelationshipStatus() {
+		return relationshipStatus;
+	}
+
+	public void setRelationshipStatus(RelationshipStatus relationshipStatus) {
+		this.relationshipStatus = relationshipStatus;
+	}
+
+	public enum RelationshipStatus {
+		Bestie, Friend
+	}
+}
