f66f489ad4a6c0148224ffd5f3145fb717b6172d
Daniel Hsu <hsu2005715@gmail.com>
2024-09-27 16:31:55 +0800

    修改user repo

---
+++ src/main/java/com/aieverywhere/backend/repostories/UserRepo.java
@@ -0,0 +1,13 @@
+package com.aieverywhere.backend.repostories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import com.aieverywhere.backend.models.Users;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer>, JpaSpecificationExecutor<Users> {
+	Users findByUsername(String username);
+	Users findByUserId(int userid);
+}
