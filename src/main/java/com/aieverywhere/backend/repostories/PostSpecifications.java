0ff160bfb8b0358ed5474cf725231270bfc541c0
Daniel Hsu <hsu2005715@gmail.com>
2024-10-04 16:28:20 +0800

    查詢方法設置

--- src/main/java/com/aieverywhere/backend/repostories/PostSpecifications.java
+++ src/main/java/com/aieverywhere/backend/repostories/PostSpecifications.java
@@ -1,11 +1,13 @@
 package com.aieverywhere.backend.repostories;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import org.springframework.data.jpa.domain.Specification;
 
 import com.aieverywhere.backend.models.Posts;
+import com.aieverywhere.backend.models.Users;
+
+import jakarta.persistence.criteria.Join;
 
 public class PostSpecifications {
 	public static Specification<Posts> hasPostId(Long postId) {
@@ -25,7 +27,7 @@ public class PostSpecifications {
 			return criteriaBuilder.equal(root.get("userId"), userId);
 		};
 	}
-	
+
 	public static Specification<Posts> greaterThanCreatedAt(LocalDateTime createdAt) {
 		return (root, query, criteriaBuilder) -> {
 			if (createdAt == null) {
@@ -45,13 +47,14 @@ public class PostSpecifications {
 
 		};
 	}
-
-	public static Specification<Posts> hasGrade(Long grade) {
-		return (root, query, criteriaBuilder) -> {
-			if (grade == null) {
-				return criteriaBuilder.conjunction(); // 返回一个总是为真的 Predicate
-			}
-			return criteriaBuilder.equal(root.get("grade"), grade);
-		};
+	
+	public static Specification<Posts> userHasName(String username) {
+	    return (root, query, criteriaBuilder) -> {
+	        Join<Posts, Users> userJoin = root.join("user");
+	        return criteriaBuilder.equal(userJoin.get("username"), username);
+	    };
 	}
+
+
+	
 }
