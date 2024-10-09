3aed278f269500351dd7e39cae897d73805a9743
Rex06020 <56222959+Rex06020@users.noreply.github.com>
2024-10-09 15:24:59 +0800

    feat: allow resposing the responses

--- src/main/java/com/aieverywhere/backend/services/ResponsesServices.java
+++ src/main/java/com/aieverywhere/backend/services/ResponsesServices.java
@@ -2,24 +2,34 @@ package com.aieverywhere.backend.services;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import com.aieverywhere.backend.dto.RespResponseDTO;
 import com.aieverywhere.backend.models.Responses;
+import com.aieverywhere.backend.models.Users;
 import com.aieverywhere.backend.repostories.RespRepo;
+import com.aieverywhere.backend.repostories.RespSpecifications;
+import com.aieverywhere.backend.repostories.UserRepo;
+import com.aieverywhere.backend.repostories.UserSpecifications;
 
 @Service
 public class ResponsesServices {
 
     @Autowired
     private RespRepo respRepo;
+    private UserRepo userRepo;
 
-    public ResponsesServices(RespRepo respRepo) {
+	public ResponsesServices(RespRepo respRepo, UserRepo userRepo) {
 		this.respRepo = respRepo;
+		this.userRepo = userRepo;
 	}
 
 	// Create a new response
@@ -30,32 +40,48 @@ public class ResponsesServices {
     }
 
     // Read all responses
-    public List<Responses> getAllResponses(int page, int size) {
-    	Page<Responses> respPage = respRepo.findAll(PageRequest.of(page, size));
+    public Map<String, Object> getPagedResponsesByPostId(Long postId, int page, int size) {
+    	Specification<Responses> resSpec = RespSpecifications.hasPostId(postId);
+    	Page<Responses> respPage = respRepo.findAll(resSpec, PageRequest.of(page, size));
         
-    	List<Responses> respList = new ArrayList<>();
+    	List<RespResponseDTO> respList = new ArrayList<>();
     	for (Responses resp : respPage) {
     		Long responseId = resp.getResponseId();
-    		Long postId = resp.getPostId();
     		Long userId = resp.getUserId();
     		String content = resp.getContent();
+    		
+    		Users user = userRepo.getReferenceById(userId);
+    		String nickname = user.getNickName();
+    		String imagePath = user.getImagePath();
+    		
     		Long likes = resp.getLikes();
     		LocalDateTime createdAt = resp.getCreatedAt();
     		LocalDateTime updateAt = resp.getUpdateAt();
     		
-    		respList.add(new Responses(responseId, postId, userId, content, likes, createdAt, updateAt));
+    		respList.add(new RespResponseDTO(responseId, postId, userId, content, nickname, imagePath, likes, createdAt, updateAt));
     	}
-    	return respRepo.findAll();
+    	
+    	Long totalItems = respRepo.count();
+    	int totalPages = (int) Math.ceil((double) totalItems / size);
+    	
+    	Map<String, Object> respMap = new HashMap<>();
+    	respMap.put("respList", respList);
+    	respMap.put("currentPage", page);
+    	respMap.put("totalItems", totalItems);
+		respMap.put("totalPages", totalPages);
+		
+    	return respMap;
     }
     
  // get all responses by postId
     public List<Responses> getAllResponsesByPostId(Long postId) {
-        return respRepo.findAllByPostId(postId);
+    	Specification<Responses> spec = RespSpecifications.hasPostId(postId);
+    	return respRepo.findAll(spec);
     }
 
     // Read a single response by ID
     public Responses getResponseById(Long id) {
-        return respRepo.findByResponseId(id);
+        return respRepo.getReferenceById(id);
     }
 
     // Update an existing response
